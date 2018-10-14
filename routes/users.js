76c0906281dd7f5d368a9230e840956b98be6df1
Md. Asadul Islam <asad.cse.ru.15@gmail.com>
2018-10-14 16:20:11 +0600

    User Login

--- routes/users.js
+++ routes/users.js
@@ -7,57 +7,93 @@ var emailKey = "^5F&hs";
 var userNameKey = "Uf%f2";
 var passwordKey = "&hs#D6";
 
-// Register User
+/******* User Registration ****************/
+/******************************************/
 router.post('/register', function(req, res, next) {
-    var name = req.body.name;
-    var email = req.body.email;
-    var userName = req.body.userName;
-    var password = req.body.password;
-    var rePassword = req.body.rePassword;
-
-    // Validation
-    req.checkBody('name', 'Name is required').notEmpty();
-    req.checkBody('email', 'Email is required').notEmpty();
-    req.checkBody('email', 'Email is not valid').isEmail();
-    req.checkBody('userName', 'Username is required').notEmpty();
-    req.checkBody('password', 'Password is required').notEmpty();
-    req.checkBody('rePassword', 'Passwords do not match').equals(password);
-
-    var errors = req.validationErrors();
-
-    if (errors) {
-        console.log(errors);
-        res.send(errors);
+    if (req.body == null || req.body == undefined) {
+        res.send("Enter valid Info.")
     } else {
-        userName = crypto.encrypt(userName.toLowerCase(), userNameKey);
-        email = crypto.encrypt(email.toLowerCase(), emailKey);
-        password = crypto.encrypt(password, passwordKey);
-        //checking for email and username are already taken
-        User.findOne({
-                $or: [{ userName: userName }, { email: email }]
-            },
-            function(err, user) {
-                if (user) {
-                    res.send("User or Email is Already exits");
-                } else {
-                    var newUser = new User({
-                        name: name,
-                        email: email,
-                        userName: userName,
-                        password: password
-                    });
-                    newUser.save(newUser, function(err, user) {
-                        if (err) {
-                            res.send('Internal Error');
-                            console.log("Can't insert into database")
-                        } else {
-                            res.send('You are registered and can now login');
-                        }
-                    });
-                }
+        var name = req.body.name;
+        var email = req.body.email;
+        var userName = req.body.userName;
+        var password = req.body.password;
+        var rePassword = req.body.rePassword;
 
+        // Validation
+        req.checkBody('name', 'Name is required').notEmpty();
+        req.checkBody('email', 'Email is required').notEmpty();
+        req.checkBody('email', 'Email is not valid').isEmail();
+        req.checkBody('userName', 'Username is required').notEmpty();
+        req.checkBody('password', 'Password is required').notEmpty();
+        req.checkBody('rePassword', 'Passwords do not match').equals(password);
+
+        var errors = req.validationErrors();
+
+        if (errors) {
+            console.log(errors);
+            res.send(errors);
+        } else {
+            userName = crypto.encrypt(userName.toLowerCase(), userNameKey);
+            email = crypto.encrypt(email.toLowerCase(), emailKey);
+            password = crypto.encrypt(password, passwordKey);
+            //checking for email and username are already taken
+            User.findOne({
+                    $or: [{ userName: userName }, { email: email }]
+                },
+                function(err, user) {
+                    if (user) {
+                        res.send("User or Email is Already exits");
+                    } else {
+                        var newUser = new User({
+                            name: name,
+                            email: email,
+                            userName: userName,
+                            password: password
+                        });
+                        newUser.save(newUser, function(err, user) {
+                            if (err) {
+                                res.send('Internal Error');
+                                console.log("Can't insert into database")
+                            } else {
+                                res.send('You are registered and can now login');
+                            }
+                        });
+                    }
+
+                });
+        }
+    }
+});
+
+/************ User Login **********/
+/**********************************/
+router.post("/login", function(req, res, next) {
+    if (req.body == null || req.body == undefined) {
+        res.send("Enter Valid Info")
+    } else {
+        var email = req.body.email;
+        var password = req.body.password;
+
+        req.checkBody("email", "Email is empty");
+        req.checkBody("password", "Password is empty");
+
+        var errors = req.validationErrors();
+
+        if (errors) {
+            res.send(errors);
+        } else {
+            email = crypto.encrypt(email.toLowerCase(), emailKey);
+            password = crypto.encrypt(password, passwordKey);
+            User.findOne({ email: email, password: password }, function(err, user) {
+                if (user) {
+                    res.send("Login successful");
+                } else {
+                    res.send("Email or password is wrong");
+                }
             });
+        }
     }
 });
 
 module.exports = router;
+/**********************************/
