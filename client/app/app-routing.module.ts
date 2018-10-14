b6e36148b7faa805159ae0d73d949a1efcdb3c28
Md. Asadul Islam <asad.cse.ru.15@gmail.com>
2018-10-14 17:54:15 +0600

    Client side pages

--- client/app/app-routing.module.ts
+++ client/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { Error404Component } from './error404/error404.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: "register", component: RegisterComponent },
+  { path: "login", component: LoginComponent },
+  { path: "dashboard", component: DashboardComponent },
+  { path: '', component: HomeComponent },
+  { path: '**', component: Error404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Error404Component } from './error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
