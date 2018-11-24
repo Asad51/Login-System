1c38352ddb72f90f0e1f5e584c1eab5fc2fe0cb8
Md. Asadul Islam <asad.cse.ru.15@gmail.com>
2018-11-25 00:46:06 +0600

    signup using async/await

--- controllers/signup.js
+++ controllers/signup.js
@@ -2,101 +2,106 @@ var User = require('../models/users');
 var crypto = require('../libs/hashing');
 var secretKeys = require('../config/secret.keys.js');
 
+async function findUser(query, res) {
+    let user = await User.findOne(query)
+        .catch((err) => {
+            res.send("Server Error");
+        });
+    return user;
+}
+
+function createUser(user, req, res) {
+    user.save(user, function(err, user) {
+        if (err) {
+            res.status(500).send({ "error_msg": 'Internal Error' });
+            console.log("Can't insert into database")
+        } else {
+            req.flash('success_msg', 'You are registered and can now login');
+            res.redirect('/signin');
+        }
+    });
+}
+
 module.exports = {
-	signupPage: function(req, res, next){
-		res.status(200).send({ "title": "Create New Account" });
-	},
+    signupPage: function(req, res, next) {
+        res.status(200).send({ "title": "Create New Account" });
+    },
 
-	userInfo: (req, res, next) => {
-		if (Object.keys(req.body).length !== 5) {
-	        res.send("Invalid format");
-	    } else {
-	        var name = req.body.name;
-	        var email = req.body.email;
-	        var userName = req.body.userName;
-	        var password = req.body.password;
-	        var confirmPassword = req.body.confirmPassword;
+    userInfo: async function(req, res, next) {
+        if (Object.keys(req.body).length !== 5) {
+            res.send("Invalid format");
+        } else {
+            var name = req.body.name;
+            var email = req.body.email;
+            var userName = req.body.userName;
+            var password = req.body.password;
+            var confirmPassword = req.body.confirmPassword;
 
-	        // Validation
-	        req.checkBody('name', 'Name is required').notEmpty();
-	        req.checkBody('email', 'Email is required').notEmpty();
-	        req.checkBody('email', 'Email is not valid').isEmail();
-	        req.checkBody('userName', 'Username is required').notEmpty();
-	        req.checkBody('password', 'Password is required').notEmpty();
-	        req.checkBody('confirmPassword', 'Passwords do not match').equals(password);
+            // Validation
+            req.checkBody('name', 'Name is required').notEmpty();
+            req.checkBody('email', 'Email is required').notEmpty();
+            req.checkBody('email', 'Email is not valid').isEmail();
+            req.checkBody('userName', 'Username is required').notEmpty();
+            req.checkBody('password', 'Password is required').notEmpty();
+            req.checkBody('confirmPassword', 'Passwords do not match').equals(password);
+            var errors = req.validationErrors();
 
-	        var errors = req.validationErrors();
+            if (errors) {
+                res.send(errors);
+            } else {
+                userName = crypto.encrypt(userName.toLowerCase(), secretKeys.userNameKey, secretKeys.userNameIV);
+                email = crypto.encrypt(email.toLowerCase(), secretKeys.emailKey, secretKeys.emailIV);
+                password = crypto.encrypt(password, secretKeys.passwordKey);
 
-	        if (errors) {
-	            res.send(errors);
-	        } else {
-	            userName = crypto.encrypt(userName.toLowerCase(), secretKeys.userNameKey, secretKeys.userNameIV);
-	            email = crypto.encrypt(email.toLowerCase(), secretKeys.emailKey, secretKeys.emailIV);
-	            password = crypto.encrypt(password, secretKeys.passwordKey);
-	            //checking for email and username are already taken
-	            User.findOne({
-	                    $or: [{ userName: userName }, { email: email }]
-	                },
-	                function(err, user) {
-	                    if (user) {
-	                        res.send("User or Email is Already exits");
-	                    } else {
-	                        var newUser = new User({
-	                            name: name,
-	                            email: email,
-	                            userName: userName,
-	                            password: password
-	                        });
-	                        newUser.save(newUser, function(err, user) {
-	                            if (err) {
-	                                res.status(500).send({ "error_msg": 'Internal Error' });
-	                                console.log("Can't insert into database")
-	                            } else {
-	                                req.flash('success_msg', 'You are registered and can now login');
-	                                res.redirect('/signin');
-	                            }
-	                        });
-	                    }
+                let user = await findUser({ userName: userName }, res);
+                let ep = {};
+                if (user) {
+                    ep.userName = "Username is exist";
+                }
+                users = await findUser({ email: email }, res);
+                if (user) {
+                    ep.email = "Email is exist";
+                }
 
-	                });
-	        }
-	    }
-	},
+                if (ep) {
+                    res.send(ep);
+                } else {
+                    var newUser = new User({
+                        name: name,
+                        email: email,
+                        userName: userName,
+                        password: password
+                    });
+                    createUser(newUser, req, res);
+                }
+            }
+        }
+    },
 
-	userCheck: (req, res, next) =>{
-		if (Object.keys(req.body).length !== 1) {
-	        res.send("Invalid Format")
-	    } else {
-	        var email = req.body.email;
-	        var userName = req.body.userName;
+    userCheck: (req, res, next) => {
+        if (Object.keys(req.body).length !== 1) {
+            res.send("Invalid Format")
+        } else {
+            var email = req.body.email;
+            var userName = req.body.userName;
 
-	        if (!userName && !email) {
-	            res.send({ "error_msg": "Enter valid info" });
-	        } else {
-	            if (userName) {
-	                userName = crypto.encrypt(userName.toLowerCase(), secretKeys.userNameKey, secretKeys.userNameIV);
-	                User.findOne({ userName: userName }, function(err, user) {
-	                    if (user) {
-	                        res.send("Username is already used.");
-	                    } else {
-	                        res.status(200).send({ "success_msg": "" });
-	                    }
-	                });
-	            } else {
-	                email = crypto.encrypt(email.toLowerCase(), secretKeys.emailKey, secretKeys.emailIV);
-	                User.findOne({ email: email }, function(err, user) {
-	                    if (user) {
-	                        res.send("Email is already used.");
-	                    } else {
-	                        res.status(200).send({ "success_msg": "" });
-	                    }
-	                })
-	            }
-
-	        }
-
-	    }
-	}
+            if (!userName && !email) {
+                res.send({ "error_msg": "Enter valid info" });
+            } else {
+                if (userName) {
+                    userName = crypto.encrypt(userName.toLowerCase(), secretKeys.userNameKey, secretKeys.userNameIV);
+                    if (findUser({ userName: userName })) {
+                        res.send("Username is already used.");
+                    }
+                } else {
+                    email = crypto.encrypt(email.toLowerCase(), secretKeys.emailKey, secretKeys.emailIV);
+                    if (findUser({ email: email })) {
+                        res.send("Email is already used.");
+                    }
+                }
+            }
+        }
+    }
 }
 
 /**********************************/
